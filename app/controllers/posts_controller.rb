e192fbe4c4078934266c5e69187836debab3ba8b
KSTMike <katomikako04@gmail.com>
2023-06-06 18:13:25 +0900

    再提出

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,64 +1,60 @@
 class PostsController < ApplicationController
-    def index
-        @posts = Post.all
-    end
+  def index
+		@posts = Post.all
+  end
 
-    def new
-        @post = Post.new
-    end
+  def new
+		@post = Post.new
+  end
 
-    def create
-        @post = Post.new(post_params)
-        if params[:back]
-            render :new
-        else        
-          if @post.save
-            redirect_to posts_path, notice: "Tubuyakuuを作成しました！"  
-          else
-            render :new
-          end
-        end 
-    end
+  def create
+		@post = Post.new(post_params)
+		if params[:back]
+				render :new
+		else        
+			if @post.save
+				redirect_to posts_path, notice: "Tubuyakuuを作成しました！"  
+			else
+				render :new
+			end
+		end 
+  end
 
-    def show
-        params[:id]
-        @post = Post.find(params[:id])
-    end
+	def show
+		params[:id]
+		@post = Post.find(params[:id])
+	end
     
-    def edit
-        @post = Post.find(params[:id])
-    end
+	def edit
+		@post = Post.find(params[:id])
+	end
 
-    def confirm
-        @post = Post.new(post_params)
-        render :new if @post.invalid?
-    end 
+	def confirm
+		@post = Post.new(post_params)
+		render :new if @post.invalid?
+	end 
 
-    def update
-        @post = Post.find(params[:id]) # 編集したい投稿のidを取得
-        if @post.update(post_params) # その投稿を更新
-            redirect_to posts_path, notice: "Tubuyakuuを編集しました！"
-        else
-            render :edit
-        end
-    end
+	def update
+		@post = Post.find(params[:id]) # 編集したい投稿のidを取得
+		if @post.update(post_params) # その投稿を更新
+			redirect_to posts_path, notice: "Tubuyakuuを編集しました！"
+		else
+			render :edit
+		end
+	end
     
-    def destroy
-        @post = Post.destroy(params[:id])
-        redirect_to posts_path, notice: "Tubuyakuuを削除しました！"
-    end
+	def destroy
+		@post = Post.destroy(params[:id])
+		redirect_to posts_path, notice: "Tubuyakuuを削除しました！"
+	end
 
-    def link_to
-        @post = Post.find(params[:id])
-    end
+	def set_post
+		@post = Post.find(params[:id])
+	end
 
-    def set_post
-        @post = Post.find(params[:id])
-    end
+	private
 
-    private
-
-    def post_params
-        params.require(:post).permit(:content)
-    end
+	def post_params
+		params.require(:post).permit(:content)
+	end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
-    validates :content, presence: true
-    # validates :user_name, uniqeness: true
-    validates :content, length: {minimum: 1 }
-    validates :content, length: {maximum: 140}
+  validates :content, presence: true
+  validates :content, length: {minimum: 1 }
+  validates :content, length: {maximum: 140}
 end
